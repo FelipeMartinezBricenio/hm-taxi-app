21a5f9d724a0e5a010ec989a48bf82cc1e7af450
FelipeMartinezBricenio <felipemb513@gmail.com>
2026-04-21 21:36:33 -0500

    Add files via upload
    aaa

--- sw.js
+++ sw.js
@@ -1,22 +1,36 @@
-const CACHE_NAME = 'hm-taxi-v1';
+const CACHE_NAME = 'hm-taxi-v2';
 const assets = [
   '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/logo.png'
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json',
+  'logo.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
+// Instalar Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+// Activar y limpiar cachés antiguos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estrategia: Primero Red, si falla, Caché
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
